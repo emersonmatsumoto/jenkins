ba2f708fc29ca4e8da8f96c351e2ba06314e86fe
Emerson <japinha02@gmail.com>
2018-01-28 14:35:39 -0200

    Inicial

---
+++ README.md
@@ -0,0 +1,12 @@
+EC2 crie uma nova instância Amazon Linux 2
+
+Crie um security group com a porta 8080 liberada
+
+Logue na máquina
+$ssh -i jenkins.pem ec2-user@34.228.73.40
+
+Baixe o script
+wget 
+
+
+
---
+++ jenkins.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+clear
+
+echo "Instalando openjdk 8"
+yum install java-1.8.0-openjdk-devel -y
+
+echo "Instalando maven 3"
+wget http://mirror.nbtelecom.com.br/apache/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz
+tar xvf apache-maven-3.5.2-bin.tar.gz
+mv apache-maven-3.5.2 /usr/local/apache-maven
+cat >/etc/profile.d/maven.sh <<EOL
+export M2_HOME=/usr/local/apache-maven
+export M2=$M2_HOME/bin
+export PATH=$M2:$PATH
+EOL
+
+echo "Instalando jenkins"
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum install jenkins
+service jenkins start
+cat /var/lib/jenkins/secrets/initialAdminPassword
