5259c25b29f4dea3c4b05d4c9509131e9b56b556
Emerson <japinha02@gmail.com>
2018-01-28 14:57:29 -0200

    Ajustes no script

--- jenkins.sh
+++ jenkins.sh
@@ -15,6 +15,9 @@ export PATH=\$M2:$PATH
 EOL
 source /etc/profile.d/maven.sh
 
+echo "Instalando git"
+yum install git -y
+
 echo "Instalando jenkins"
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
